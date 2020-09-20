c018ea81fd39a6a74503addd3a00ba92d14d921e
apadhi <padhiarunanshu@gmail.com>
2020-09-21 02:06:53 +0530

    Updated with an interview question

--- 50_CPP_problems.cpp
+++ 50_CPP_problems.cpp
@@ -1,4 +1,4 @@
-// Q: what is the order of execution of new, c'tor, d'tor and delete in cpp
+// Q1: what is the order of execution of new, c'tor, d'tor and delete in cpp
 #include <iostream>
 using namespace std;
 
@@ -21,7 +21,7 @@ class A {
 };
 
 // ----------------------------------------------------------------------
-// Q: Design final class in c++ 
+// Q2: Design final class in c++ 
 
 class Final;
 class MakeFinal {
@@ -37,6 +37,26 @@ class Final : virtual public MakeFinal {
 };
 
 class Derived : public Final {};
+
+// ----------------------------------------------------------------------
+// Q3: Delete this?
+
+class A {
+  public:
+	A() { cout << "A::A()\n"; }
+	~A() { cout << "A::~A()\n"; }
+
+	void fun() { // calling this function will delete this
+		cout << "Delete this\n";
+		delete this;
+	}
+
+	// this will prevent the program to crash if an obect is created in stack and fun is called on that.
+	void operator delete(void*) {
+		cout << "operator delete\n";
+		return ;
+	}
+};
 
 // **************************************************************************************** //
 int main() {
